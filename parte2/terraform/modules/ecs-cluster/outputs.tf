b20d9737cd625bf82e9de23babdd74e276517a80
jonathan.m.lucena@gmail.com <jonathan.m.lucena@gmail.com>
2021-10-17 16:23:50 -0300

    feat: ecs cluster module

---
+++ parte2/terraform/modules/ecs-cluster/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_ecs_cluster_cluster_name" {
+  value = aws_ecs_cluster.cluster.name
+}
+
+output "aws_ecs_cluster_cluster_id" {
+  value = aws_ecs_cluster.cluster.id
+}
+
+output "aws_ecs_cluster_cluster_arn" {
+  value = aws_ecs_cluster.cluster.arn
+}
