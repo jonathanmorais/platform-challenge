636449c1b17d24f08ab3c481bbcbde122c897183
jonathan.m.lucena@gmail.com <jonathan.m.lucena@gmail.com>
2021-10-19 17:25:24 -0300

    feat: terraform for helm release

---
+++ parte1/helm/terraform/main.tf
@@ -0,0 +1,33 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.default.token
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "test-terraform-jonathan"
+    key    = "terraform/app-test/helm/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+
+resource "helm_release" "demo-app" {
+  name  = "${var.service}-chart"
+  chart = "../${var.service}"
+  wait = false
+}
---
+++ parte1/helm/terraform/stg.tfvars
@@ -0,0 +1,2 @@
+cluster_name = "cluster-test"
+service      = "app-helloworld"
